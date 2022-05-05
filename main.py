11f5bd6a476f166b3b90dadde66dd0fa5b441f7f
Samuel Insley <104997413+Talkabl@users.noreply.github.com>
2022-05-05 14:53:34 +0000

    Nature Valleys.

---
+++ main.py
@@ -0,0 +1,24 @@
+
+import random
+
+def diceRollP1():
+ return random.randint(1,6)
+
+def diceRollP2():
+ return random.randint(1,6)
+
+
+player1 = diceRollP1()
+player2 = diceRollP2()
+
+print(f'player 1 rolled a {player1}')
+print(f'player 2 rolled a {player2}')
+
+if player1 > player2:
+  print('player 1 wins')
+
+if player2 > player1:
+  print('player 2 wins')
+
+if player1 == player2:
+  print('draw')
